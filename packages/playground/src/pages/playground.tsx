93ce0ec5f4ddc8aabb78fdd481fd1e9983209f28
hardfist <1562502418@qq.com>
2021-02-14 22:50:15 +0800

    feat(playground): add http import

--- packages/playground/src/pages/playground.tsx
+++ packages/playground/src/pages/playground.tsx
@@ -12,6 +12,7 @@ import { ListItem } from '../components/list';
 const initialFiles = {
   'main.js': `
   import './style.css'
+  import react from 'https://unpkg.com/react@17.0.1/index.js'
   import answer from 'the-answer';
   import * as lib from './lib';
   console.log('lib:',lib);
